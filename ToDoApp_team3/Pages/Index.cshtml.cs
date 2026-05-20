d7cdf264ac988217e0d52c1512871df234eb382d
shibuyayuki0 <yshibuya@youtheng.co.jp>
2026-05-20 11:09:20 +0900

    SQL更新のためcommit

--- ToDoApp_team3/Pages/Index.cshtml.cs
+++ ToDoApp_team3/Pages/Index.cshtml.cs
@@ -1,20 +1,48 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ToDoApp_team3.Model;
 
 namespace ToDoApp_team3.Pages
 {
     public class IndexModel : PageModel
     {
+        [BindProperty(SupportsGet = true)]
+        public string FilterName { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int SelectedId { get; set; }
+
+        public List<Task> TaskList { get; set; }
+        public List<Priorities> PriorityList { get; set; }
+
         public List<(int id, string name)> ItemList { get; set; } = [];
 
         public DateTime DeadlineAt => DateTime.Now;
 
         public void OnGet()
         {
-            for(int i = 1; i <= 10; i++)
+            TaskList = new List<Task>();
+            PriorityList = new List<Priorities>();
+
+            for (int i = 1; i <= 10; i++)
             {
-                ItemList.Add((i,$"ID：{i}のタスク"));
+                ItemList.Add((i, $"ID：{i}のタスク"));
             }
         }
+
+        public void OnGetFiltering()
+        {
+            TaskList = new List<Task> ();
+        }
+
+        //public IActionResult OnPostDelete()
+        //{
+
+        //}
+
+        //public IActionResult OnPostComplete()
+        //{
+
+        //}
     }
 }
